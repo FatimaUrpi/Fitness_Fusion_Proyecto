0b0652ee5a50accc0b129dca56c4b8ef77a66c7b
AstridYovera <ayoveratinoco@gmail.com>
2024-11-04 16:05:02 -0500

    feat: iniciar sesion actualizado

--- JS/Sesion/inciarSesion.js
+++ JS/Sesion/inciarSesion.js
@@ -1,6 +1,6 @@
 function togglePasswordVisibility() {
-    var passwordField = document.getElementById("passwordField");
-    var icon = document.querySelector(".toggle-password i");
+    const passwordField = document.getElementById("contraseña");
+    const icon = document.querySelector(".toggle-password i");
 
     if (passwordField.type === "password") {
         passwordField.type = "text";
@@ -13,24 +13,15 @@ function togglePasswordVisibility() {
     }
 }
 
+// Función para el placeholder
+function togglePlaceholder(input, isFocused) {
+    if (isFocused) {
+        input.placeholder = '';
+    } else if (!input.value) {
+        input.placeholder = input.getAttribute('data-placeholder');
+    }
+}
 
-
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('incioSesion-form');
-    form.addEventListener('submit', function (event) {
-        event.preventDefault(); // Evitar que el formulario se envíe de forma predeterminada
-        
-        // Aquí puedes agregar tu lógica para guardar los datos en JSON
-        
-        // Luego, muestra la alerta
-        Swal.fire({
-            title: "Inicio de Sesión exitoso!",
-            text: "Bienvenito a FITNESS FUSION!!",
-            icon: "success"
-        }).then(() => {
-            // Redirigir a la página de inicio
-            window.location.href = "Index.html";
-        });
-    });
+document.querySelectorAll('input[type="text"], input[type="password"]').forEach(input => {
+    input.setAttribute('data-placeholder', input.placeholder);
 });
-
